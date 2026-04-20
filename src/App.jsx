2de3b8fb63b2ddbabdf7b1ae930f1c0349a922c8
Wadim1970 <fomenko1970@yandex.ru>
2026-04-20 09:49:51 +0300

    Initialize App component with routing setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import WelcomeScreen from './components/WelcomeScreen'
+import './App.css'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WelcomeScreen />} />
+        <Route path="/login" element={<div>Login Screen (скоро)</div>} />
+        <Route path="/register" element={<div>Register Screen (скоро)</div>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
