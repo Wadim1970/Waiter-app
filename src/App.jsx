efecbe590cca810eb81f39cfff12f8391d193df3
Wadim1970 <fomenko1970@yandex.ru>
2026-04-21 15:03:05 +0300

    Replace register screen placeholder with RegisterScreen

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<WelcomeScreen />} />
         <Route path="/login" element={<div>Login Screen (скоро)</div>} />
-        <Route path="/register" element={<div>Register Screen (скоро)</div>} />
+        <Route path="/register" element={<RegisterScreen />} />
       </Routes>
     </BrowserRouter>
   )
