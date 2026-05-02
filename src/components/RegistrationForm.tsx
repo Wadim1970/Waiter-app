3a54b1dd687f9847046e81699e7c1e32687347ea
Wadim1970 <fomenko1970@yandex.ru>
2026-05-02 12:40:41 +0300

    Remove unused import of useSearchParams

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react'
-import { useNavigate, useSearchParams } from 'react-router-dom'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import styles from './RegistrationForm.module.css'
 
 export default function RegistrationForm() {
